685897b2ed36ba7c2c104bbb23fc7128cad9fbf8
d9647 <150551387+d9647@users.noreply.github.com>
2025-12-28 19:28:13 -0800

    Delete scripts/run-web.sh

--- scripts/run-web.sh
+++
@@ -1,9 +0,0 @@
-#AAPL,MSFT,GOOGL,AMZN,NVDA,META,TSLA
-cd ../web
-pnpm install
-echo $NEXT_PUBLIC_API_URL
-# for development mode
-#pnpm dev
-# below is for production mode
-rm -rf .next
-npm run build
