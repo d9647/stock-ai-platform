f8064d5fdb7c24a577e9959ab4d49ca63031a0a2
d9647 <150551387+d9647@users.noreply.github.com>
2025-12-28 19:29:10 -0800

    Delete scripts/run-3.sh

--- scripts/run-3.sh
+++
@@ -1,7 +0,0 @@
-#AAPL,MSFT,GOOGL,AMZN,NVDA,META,TSLA
-cd ../services/feature-store
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-#python -m src.pipelines.daily_feature_pipeline --ticker AAPL --days 365
-python -m src.pipelines.daily_feature_pipeline --ticker MSFT --days 365
