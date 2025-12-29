7ef9c8615ab07804729344a2b86963cff3914a2b
d9647 <150551387+d9647@users.noreply.github.com>
2025-12-28 19:29:25 -0800

    Delete scripts/run-api.sh

--- scripts/run-api.sh
+++
@@ -1,9 +0,0 @@
-#AAPL,MSFT,GOOGL,AMZN,NVDA,META,TSLA
-cd ../api
-source venv/bin/activate
-python -m app.main
-
-#curl http://192.168.5.126:8000/api/v1/health
-#cd api
-#source venv/bin/activate
-#uvicorn app.main:app --host 0.0.0.0 --port 8000
